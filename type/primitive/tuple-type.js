5956108ba9d54d047f4015335aa06b30158fd43a
liuxili01 <liuxili@bestseller.com.cn>
2022-07-21 10:38:03 +0800

    基础类型

---
+++ type/primitive/tuple-type.js
@@ -0,0 +1,9 @@
+//定义一个元组
+let x;
+//对元组进行初始化
+x = ['hello', 10];
+x.push(12);
+console.log(x);
+console.log(x[0]);
+console.log(x[1]);
+console.log(x.pop());
---
+++ type/primitive/tuple-type.ts
@@ -0,0 +1,11 @@
+//定义一个元组
+let x:[string,number];
+//对元组进行初始化
+x=['hello',10];
+
+x.push(12)
+console.log(x);
+console.log(x[0]);
+console.log(x[1]);
+
+console.log(x.pop());
