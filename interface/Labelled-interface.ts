b4a0ba6b5cedb0f3d4c1f39bb3dd424fd0a4bd3e
liuxili01 <liuxili@bestseller.com.cn>
2022-07-22 12:08:26 +0800

    接口

---
+++ interface/Labelled-interface.ts
@@ -0,0 +1,14 @@
+//LabelledValue接口就好比一个名字，用来描述上面例子里的要求。 它代表了有一个 label属性且类型为string的对象。 需要注意的是，我们在这里并不能像在其它语言里一样，说传给 printLabel的对象实现了这个接口。我们只会去关注值的外形。 只要传入的对象满足上面提到的必要条件，那么它就是被允许的。
+
+interface LabelledValue{
+    label:string;
+}
+
+function printLabel(labelledObj:LabelledValue){
+    console.log(labelledObj.label);
+}
+
+
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
---
+++ interface/basic-interface.ts
@@ -0,0 +1,11 @@
+//类型检查器会查看printLabel的调用。 printLabel有一个参数，并要求这个对象参数有一个名为label类型为string的属性。 需要注意的是，我们传入的对象参数实际上会包含很多属性，但是编译器只会检查那些必需的属性是否存在，并且其类型是否匹配
+
+function printLabel(labelledObj: { label: string }) {
+    console.log(labelledObj.label);
+}
+
+let myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+
+
+export {};
